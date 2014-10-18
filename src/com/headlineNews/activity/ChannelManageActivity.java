51ef0742802d83afe5f9d013d5d6abd6b5cb4d29
jake <1832702206@qq.com>
2014-10-18 11:45:13 +0800

    项目框架 第三次提交   曾令鹏

---
+++ src/com/headlineNews/activity/ChannelManageActivity.java
@@ -0,0 +1,13 @@
+package com.headlineNews.activity;
+
+import android.os.Bundle;
+
+import com.headlineNews.baseActivity.BaseActivity;
+
+public class ChannelManageActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
