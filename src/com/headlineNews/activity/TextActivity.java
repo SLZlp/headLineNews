f836c02f75e8626a3c9b9cf3951f7e73016cc2e2
jake <1832702206@qq.com>
2014-10-18 15:04:10 +0800

    测试用

---
+++ src/com/headlineNews/activity/TextActivity.java
@@ -0,0 +1,12 @@
+package com.headlineNews.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TextActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
